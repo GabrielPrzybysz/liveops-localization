0e4fbb88fabdb5be3403990c747c0e04007c6eb7
Gabriel Przybysz <gabriel.junior@aquiris.com.br>
2022-01-08 22:24:21 -0300

    Download raw csv file added

--- LiveOpsLocalizationUnityProject/Assets/LocalizationDownloader.cs
+++ LiveOpsLocalizationUnityProject/Assets/LocalizationDownloader.cs
@@ -1,18 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class LocalizationDownloader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private const string CSV_URL = "";
+    
+    private static string _rawLocalizationCsv;
+
+    public static string RawLocalization => _rawLocalizationCsv;
+
+    void OnEnable()
     {
-        
+        DontDestroyOnLoad(this);
+        StartCoroutine(DownloadLocalization());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator DownloadLocalization()
     {
-        
+        using (UnityWebRequest client = UnityWebRequest.Get(CSV_URL))
+        {
+            UnityWebRequestAsyncOperation result = client.SendWebRequest();
+
+            // TODO: Start loading screen
+            
+            yield return new WaitUntil(() => result.isDone);
+            
+            // TODO: End loading screen
+            
+            _rawLocalizationCsv = result.webRequest.downloadHandler.text;
+        }
     }
 }
